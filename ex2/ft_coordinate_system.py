4cac4fca8f8e45df18514da48467357863008242
Artem Sokolov <artemschannel2@gmail.com>
2026-05-29 15:00:10 +0200

    ex2: Added logic for first tuple and cal_distance(t1, t2)

--- ex2/ft_coordinate_system.py
+++ ex2/ft_coordinate_system.py
@@ -1,4 +1,4 @@
-import sys
+import math
 
 
 def tuple_parser(input_str: str) -> tuple | None:
@@ -21,18 +21,36 @@ def tuple_parser(input_str: str) -> tuple | None:
 
 def input_parser() -> tuple | None:
     try:
-        input_str = input("Enter new coordinates as floats in format 'x,y,z': ")
+        input_str = input(
+            "Enter new coordinates as floats in format 'x,y,z': "
+            )
     except EOFError:
         print("Error occurred: EOFError. Dont input EOF pls")
         return
     return tuple_parser(input_str)
 
 
+def cal_distance(t0: tuple, t1: tuple) -> float:
+    return math.sqrt(
+        (t1[0] - t0[0])**2 +
+        (t1[1] - t0[1])**2 +
+        (t1[2] - t0[2])**2
+    )
+
+
+def display_t1(t1: tuple) -> None:
+    print(f"Got a first tuple: {t1}")
+    print(f"It includes: X={t1[0]}, Y={t1[1]}, Z={t1[2]}")
+    t0 = (0.0, 0.0, 0.0)
+    print(f"Distance to center: {round(cal_distance(t0, t1), 4)}\n")
+
+
 def main() -> None:
-    print("=== Game Coordinate System ===")
-    result = input_parser()
-    while not result:
-        result = input_parser()
+    print("=== Game Coordinate System ===\n")
+    t1 = input_parser()
+    while not t1:
+        t1 = input_parser()
+    display_t1(t1)
 
 
 if __name__ == "__main__":
